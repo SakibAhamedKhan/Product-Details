c5aa981bf80441aa0a5064566b57146af3bd494c
Sakib Ahamed Khan <96885912+SakibAhamedKhan@users.noreply.github.com>
2022-06-05 01:42:17 +0600

    Product Gallery is done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,86 @@
+import React, { useEffect, useReducer } from 'react';
+import ProductDetails from './Components/ProductDetails';
+import ProductGallery from './Components/ProductGallery';
+import './App.css';
+
+export const productContext = React.createContext();
+
+const initialState = {
+  loading: true,
+  error: '',
+  product: {},
+}
+
+const initialPicture = {
+  url: ''
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SUCCESS':
+      return {
+        loading: false,
+        error: '',
+        product: action.data
+      }
+    case 'ERROR':
+      return {
+        loading: false,
+        error: 'There is a Error',
+        product: {}
+      }
+    case 'ImageOnMouseHover':
+      return {
+        url: action.hoverImg
+      }
+    default:
+      return state;
+  }
+}
+
+
+
 
 function App() {
+  const [product, dispatch] = useReducer(reducer, initialState);
+  const [hoverImg, dispatch2] = useReducer(reducer, initialPicture);
+  console.log(hoverImg);
+
+  useEffect(() => {
+    fetch('/Product.json')
+      .then(res => res.json())
+      .then(data => dispatch({ type: 'SUCCESS', data }))
+  }, []);
+
+  // Loading 
+  if(product.loading) {
+    return <div style={{
+      display:'flex',
+      justifyContent:'center',
+      alignItems: 'center',
+      width: '100wh',
+      height: '100vh'
+    }}>
+      <p>Loading...</p>
+    </div>
+  }
+
   return (
     <div>
-      
+      <productContext.Provider value={{ 
+          productDispatch: dispatch, 
+          product, 
+          hoverImgDispatch: dispatch2,
+          hoverImg
+      }}>
+
+          <div className='container'>
+            <ProductGallery />
+            <ProductDetails />
+          </div>
+
+      </productContext.Provider>
+
     </div>
   );
 }
---
+++ src/Components/ProductGallery.jsx
@@ -0,0 +1,58 @@
+import React, { useContext, useEffect } from 'react';
+import { SideBySideMagnifier } from 'react-image-magnifiers';
+import { MagnifierZoom } from 'react-image-magnifiers';
+import { MagnifierPreview } from 'react-image-magnifiers';
+import { MagnifierContainer } from 'react-image-magnifiers';
+import { productContext } from '../App';
+
+const ProductGallery = () => {
+    const productByContext = useContext(productContext);
+    const { loading, error, product } = productByContext.product;
+    console.log(productByContext);
+
+    return (
+        <div>
+            <h2>This is Product Gallery Part</h2>
+            
+            <SideBySideMagnifier transitionSpeed={1} fillGapLeft={20} style={{ height: '400px', width: '400px' }} alwaysInPlace={true} fillAvailableSpace={false} imageSrc={
+               productByContext.hoverImg.url.length > 0 ? 
+                productByContext.hoverImg.url
+               :
+               product.gallery[0].url
+            }>
+            </SideBySideMagnifier>
+
+
+
+            <div style={{
+                display: 'flex'
+            }}>
+                {
+                    product?.gallery.map((p, index) => <div
+                        key={index}
+                        
+                    >
+                        <img onMouseEnter={() => {
+                            productByContext.hoverImgDispatch({type:'ImageOnMouseHover', hoverImg: p.url})
+
+                        } } style={
+                        (p.url === productByContext.hoverImg.url || (productByContext.hoverImg.url.length === 0 && index===0)) ? 
+                        {
+                            border: '3px solid red' ,
+                            margin: '20px 10px 20px 0',
+                            borderRadius:'5px'
+                        } 
+                        : 
+                        {
+                            border: '3px solid white', 
+                            margin: '20px 10px 20px 0',
+                            borderRadius:'5px'
+                        } } src={p.thumb} alt="" />
+                    </div>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default ProductGallery;
